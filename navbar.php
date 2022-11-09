cbe25458996add473ab0554a2a48b60b92e434f9
etorrres <79731703+etorrres@users.noreply.github.com>
2022-11-08 20:51:09 -0600

    Erick T
    Plantilla

---
+++ navbar.php
@@ -0,0 +1,22 @@
+<aside class="left-sidebar">
+    <div class="scroll-sidebar">
+        <nav class="sidebar-nav">
+            <ul id="sidebarnav">
+                <li class="user-pro"> <a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><img src="assets/images/users/1.jpg" alt="user-img" class="img-circle"><span class="hide-menu">Prof. Mark</span></a>
+                    <ul aria-expanded="false" class="collapse">
+                        <li><a href="javascript:void(0)"><i class="ti-settings"></i> Account Setting</a></li>
+                        <li><a href="javascript:void(0)"><i class="fa fa-power-off"></i> Logout</a></li>
+                    </ul>    
+                </li>
+                <li class="nav-small-cap">--- PERSONAL</li>
+                <li> <a class="waves-effect waves-dark" href="index.php"><i class="icon-speedometer"></i><span class="hide-menu">Dashboard</span></a>
+                </li>
+                <li class="nav-small-cap">--- PROFESSIONAL</li>
+                <li> <a class="waves-effect waves-dark" href="university-events.html"><i class="ti-calendar"></i><span class="hide-menu">Event</span></a>
+                </li>
+                <li class="nav-small-cap">--- EXTRA COMPONENTS</li>
+                <li class="nav-small-cap">--- SUPPORT</li>
+            </ul>
+        </nav>
+    </div>
+</aside>
